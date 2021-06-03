67978c734fb2e994e088312845e2da4ea3ca1d19
Evgenia4444 <Pochikyan4@yandex.ru>
2021-06-03 11:43:47 +0300

    t

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const card = document.querySelector('.card_inner');
+// const card = document.querySelector('.card_inner');
 
-card.addEventListener('click', function() {
-    card.classList.toggle('is-flipped')
-})
+// card.addEventListener('click', function() {
+//     card.classList.toggle('is-flipped')
+// });
+
+document.querySelectorAll('.card_inner').forEach(item => {
+    item.addEventListener('click', event => {
+        item.classList.toggle('is-flipped')
+    })
+  })
