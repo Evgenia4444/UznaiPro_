813ee51f1c88044a703d4c17ce53b2b2bf37ffae
Evgenia4444 <Pochikyan4@yandex.ru>
2021-06-29 01:32:03 +0300

    t

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 // const card = document.querySelector('.card_inner');
 
 // card.addEventListener('click', function() {
@@ -8,6 +9,107 @@ document.querySelectorAll('.card_inner').forEach(item => {
     item.addEventListener('click', event => {
         item.classList.toggle('is-flipped');
         item.classList.toggle('lastindex')
-      
+
     })
-  })
+})
+
+
+// document.querySelectorAll('.mockupSmall1_inner').forEach(item => {
+//     item.addEventListener('scroll', event => {
+//         item.classList.toggle('is-flipped');
+
+
+//     })
+//   })
+
+
+// const scrolled = window.pageYOffset;
+// var tl = new TimelineMax({onUpdate:updatePercentage});
+// var tl2 = new TimelineMax();
+// const controller = new ScrollMagic.Controller();
+
+// tl.from('.mockupSmall1_inner', .5, { opacity: 0});
+// tl.from('span', 1, { width: 0}, "=-.5");
+// tl.from('#office', 1, {x:-200, opacity: 0,ease: Power4.easeInOut}, "=-1");
+// tl.from('#building', 1, {x:200, opacity: 0, ease: Power4.easeInOut}, "=-.7");
+
+// tl2.from("#box", 1, {opacity: 0, scale: 0});
+// tl2.to("#box", .5, {left: "20%", scale: 1.3, borderColor: 'white', borderWidth: 12, boxShadow: '1px 1px 0px 0px rgba(0,0,0,0.09)'})
+
+// const scene = new ScrollMagic.Scene({
+//   triggerElement: ".mockupRow1",
+//             triggerHook: "onLeave",
+//             duration: "100%"
+// })
+//   .setClassToggle('.is-flipped')
+//   .setPin(".mockupRow1")
+//   .setTween(tl)
+// 		.addTo(controller);
+
+// const scene2 = new ScrollMagic.Scene({
+//   triggerElement: "blockquote"
+// })
+//   .setTween(tl2)
+// 		.addTo(controller);
+
+
+// function updatePercentage() {
+//   //percent.innerHTML = (tl.progress() *100 ).toFixed();
+//   tl.progress();
+//   console.log(tl.progress());
+// }
+
+
+
+// window.addEventListener('scroll', event => {
+//     item.style.transform = `rotateY(180deg)`;
+//     item.style.opacity = `1`;
+
+
+// })
+
+// function updatePercentage() {
+//     tl.progress();
+//     console.log(tl.progress());
+// }
+
+// document.querySelectorAll('.mockupSmall1_inner').forEach(item => {
+
+
+
+// })
+
+
+const scrollOffset = 100;
+ 
+const scrollElement = document.querySelector(".mockupSmall1_inner");
+ 
+const elementInView = (el, offset = 0) => {
+  const elementTop = el.getBoundingClientRect().top;
+ 
+  return (
+    elementTop <= 
+    ((window.innerHeight || document.documentElement.clientHeight) - offset)
+  );
+};
+ 
+const displayScrollElement = () => {
+  scrollElement.classList.add('is-flipped');
+}
+ 
+const hideScrollElement = () => {
+  scrollElement.classList.remove('is-flipped');
+}
+ 
+const handleScrollAnimation = () => {
+  if (elementInView(scrollElement, scrollOffset)) {
+      displayScrollElement();
+  } else {
+    hideScrollElement();
+  }
+}
+ 
+window.addEventListener('scroll', () => {
+  handleScrollAnimation();
+})
+
